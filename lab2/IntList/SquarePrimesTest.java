988cef63c9b4fbbc24adecc6857a17504087a5f5
shenc4 <shenc4@rpi.edu>
2022-12-12 21:56:22 -0500

    complete lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -1,6 +1,8 @@
 package IntList;
 
 import static org.junit.Assert.*;
+
+import jh61b.junit.In;
 import org.junit.Test;
 
 public class SquarePrimesTest {
@@ -16,5 +18,13 @@ public class SquarePrimesTest {
         boolean changed = IntListExercises.squarePrimes(lst);
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesChen(){
+        IntList l = IntList.of(4,4,4,4,4);
+        boolean changed = IntListExercises.squarePrimes(l);
+        assertEquals("4 -> 4 -> 4 -> 4 -> 4", l.toString());
+        assertTrue(!changed);
     }
 }
